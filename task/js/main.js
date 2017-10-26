f9733be74dcf6aa4addd38f494c9e4e7e31013af
Dmitry <mitya.gorbachev.2013@mail.ru>
2017-10-26 18:58:48 +0300

    Add files via upload

--- task/js/main.js
+++ task/js/main.js
@@ -30,12 +30,25 @@ $(document).ready(function () {
             transition: "all 0.3s",
             display: "none"
         });
+        $('.accordion_item_title_block').children('span').removeClass().addClass('flaticon-angle-arrow-down');
         $('.accordion_item_title_block').css({
             borderRadius: "9px"
         });
-        $(this + ' span').addClass('flaticon-up-arrow');
+        $(this).css({
+            borderRadius: "9px 9px 0 0"
+        });
+        $(this).children('span').removeClass().addClass('flaticon-up-arrow');
         $(target).slideDown( "slow", function() {
             // Animation complete.
         });
+    });
+
+    $('.go_to').click( function(event){ // ловим клик по ссылке с классом go_to
+        event.preventDefault(); // вырубаем стандартное поведение
+        var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000); // анимируем скроолинг к элементу scroll_el
+        }
+        return false; // выключаем стандартное действие
     });
 });
